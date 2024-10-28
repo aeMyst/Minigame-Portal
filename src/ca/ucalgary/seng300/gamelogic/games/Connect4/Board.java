22e7dd0cbec7c1a14b99215f8a8b2f0ca7d7e1cc
lucas <xue.li2@ucalgary.ca>
2024-10-28 11:57:36 -0600

    Initialized connect4 in gameLogic branch

--- src/ca/ucalgary/seng300/gamelogic/games/Connect4/Board.java
+++ src/ca/ucalgary/seng300/gamelogic/games/Connect4/Board.java
@@ -3,7 +3,6 @@ package src.ca.ucalgary.seng300.gamelogic.games.Connect4;
 public class Board {
     public static int[][] createBoard(int rows, int columns) {
         return new int[rows][columns];
-
     }
 
 }
