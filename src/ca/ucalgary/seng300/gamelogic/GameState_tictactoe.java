3fc5866137c06bc8e6e1c73d65ba2f489d63356d
Alexis Newell <mmnewell@me.com>
2024-10-31 16:15:57 -0600

    tictactoe base

---
+++ src/ca/ucalgary/seng300/gamelogic/GameState_tictactoe.java
@@ -0,0 +1,16 @@
+
+package src.ca.ucalgary.seng300.gamelogic;
+
+import src.ca.ucalgary.seng300.gamelogic.tictactoe.Player;
+
+import java.util.ArrayList;
+
+    public class GameState_tictactoe {
+        private Player players;
+        public GameState_tictactoe(ArrayList<Player> player){
+
+        }
+        public Player getPlayers(){return players;}
+    }
+
+
---
+++ src/ca/ucalgary/seng300/gamelogic/games/tictactoe/Player.java
@@ -0,0 +1,21 @@
+package src.ca.ucalgary.seng300.gamelogic.tictactoe;
+
+public class Player {
+    public String[][] cells = {{"z","z","z"},{"z","z","z"},{"z","z","z"}};
+    public Player(char symbol, Board board){
+
+    }
+    public Board makeplay(Player player, Board board,int cell){
+        // amended board
+        return board;
+    }
+
+    public String getPlayer(Player player){
+        // x or o
+        return null;
+
+    }
+
+
+}
+
