65b14855f071cb6885d31be89509f1b5daf21273
lucas <xue.li2@ucalgary.ca>
2024-11-01 20:50:15 -0600

    Leaderboard restructured and implements ILeaderboard

--- src/main/java/src/ca/ucalgary/seng300/leaderboard/interfaces/ILeaderboard.java
+++ src/main/java/src/ca/ucalgary/seng300/leaderboard/interfaces/ILeaderboard.java
@@ -5,6 +5,7 @@ import src.ca.ucalgary.seng300.leaderboard.logic.EloData;
 import java.util.List;
 
 public interface ILeaderboard {
+    //we will be taking (String "whatever file path")
     void loadPlayersFromCSV();
     void addPlayer(EloData playerID);
     List<EloData> sortedLeaderboard();
--- src/main/java/src/ca/ucalgary/seng300/leaderboard/logic/Leaderboard.java
+++ src/main/java/src/ca/ucalgary/seng300/leaderboard/logic/Leaderboard.java
@@ -1,24 +1,30 @@
 package src.ca.ucalgary.seng300.leaderboard.logic;
 
 import src.ca.ucalgary.seng300.gamelogic.players.PlayerID;
+import src.ca.ucalgary.seng300.leaderboard.interfaces.ILeaderboard;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public class Leaderboard {
-    private HashMap<PlayerID, Integer> rankedPlayers;
+public class Leaderboard implements ILeaderboard {
+    private HashMap<String, EloData> players = new HashMap<>();
 
-    //Sets up hashmap to store ranked players on leaderboard
-    public Leaderboard() {
-        this.rankedPlayers = new HashMap<>();
+    @Override
+    public void loadPlayersFromCSV() {
+        //we'll try bufferedReader and parse that values we need from the csv
     }
 
-    //method for updating the leaderboard with new Elo ratings. This will be called after each match.
-    public void updateLeaderboard(PlayerID playerID, int newElo) {
-        rankedPlayers.put(playerID, newElo);
+    @Override
+    public void addPlayer(EloData playerID) {
+        //future logic implemented here
+
     }
 
-    //method for getting a player's rank.
-    public int getPlayerRank(PlayerID playerID) {
-        return rankedPlayers.getOrDefault(playerID, 1);
+    //sorts names with corresponding elo values in order
+    @Override
+    public List<EloData> sortedLeaderboard() {
+        List<EloData> sortedPlayers = new ArrayList<>(players.values());
+        return sortedPlayers;
     }
 }
