19ecb5e366b916c45cc84e399c977cca98546f17
brieabt <angelika.tanghal@ucalgary.ca>
2024-11-03 18:13:17 -0700

    LeaderboardController.java super rough draft

--- src/main/java/src/ca/ucalgary/seng300/leaderboard/controllers/LeaderboardController.java
+++ src/main/java/src/ca/ucalgary/seng300/leaderboard/controllers/LeaderboardController.java
@@ -1,4 +1,46 @@
 package src.ca.ucalgary.seng300.leaderboard.controllers;
 
+import src.ca.ucalgary.seng300.leaderboard.logic.*;
+import java.util.*;
+
+
 public class LeaderboardController {
+    private Leaderboard leaderboard;
+    private EloRating elo;
+    private MatchMaker matchmaker;
+
+    // constructor
+
+    public LeaderboardController() {
+        this.leaderboard = new Leaderboard();
+        this.elo = new EloRating();
+        this.matchmaker = new MatchMaker();
+    }
+
+    // parsing csv file
+    public void loadPlayers(String filename) {
+        leaderboard.loadPlayersFromCSV(); // depends on the loadPlayersFromCSV method 
+        displaySortedLeaderboard();
+    }
+    public void displaySortedLeaderboard() {
+        List<EloData> sorted = leaderboard.sortedLeaderboard();
+        for (EloData player : sorted) {
+            System.out.println("PlayerID: " + player.getPlayerID() + ", Elo: " + player.getElo());
+        }
+    }
+
+    // using UI to add players
+    public void addPlayer(EloData player) {
+        // implement later
+    }
+
+    // use UI to find match & create match
+    public void findMatch() {
+        matchmaker.findMatch();
+    }
+
+    public void updateEloAfterMatch(EloData winner, EloData loser) {
+        elo.updateElo(winner, loser);
+    }
+
 }
