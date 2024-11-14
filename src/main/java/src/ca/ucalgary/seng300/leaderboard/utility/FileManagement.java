bc15dcc849713a4394289d80371ba60a0b9bc4fd
Lucas Li <xue.li2@ucalgary.ca>
2024-11-13 17:22:02 -0700

    created Storage.java for info storage and initiated FileManagement

---
+++ src/main/java/src/ca/ucalgary/seng300/leaderboard/utility/FileManagement.java
@@ -0,0 +1,14 @@
+package src.ca.ucalgary.seng300.leaderboard.utility;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class FileManagement {
+
+}
