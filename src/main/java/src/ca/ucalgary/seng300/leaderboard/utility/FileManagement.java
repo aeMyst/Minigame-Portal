880cafde25efc700392ccd715582d5e8c28feac0
Lucas Li <xue.li2@ucalgary.ca>
2024-11-13 17:33:47 -0700

    added FileManagement.java and created filereading functionality

--- src/main/java/src/ca/ucalgary/seng300/leaderboard/utility/FileManagement.java
+++ src/main/java/src/ca/ucalgary/seng300/leaderboard/utility/FileManagement.java
@@ -1,4 +1,7 @@
 package src.ca.ucalgary.seng300.leaderboard.utility;
+import src.ca.ucalgary.seng300.leaderboard.data.Player;
+import src.ca.ucalgary.seng300.leaderboard.data.Storage;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -10,5 +13,33 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class FileManagement {
+    public static Storage fileReading(File file) {
+        ArrayList<Player> players = new ArrayList<>();
+        Storage storage = null;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line = reader.readLine();
+            while (line != null) {
+                String[] parts = line.split(","); //for reading csv files blah,13,13,11
+                if (parts.length == 4) {
+                    Player player = new Player(parts[0], Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), Integer.parseInt(parts[3]));
+                    players.add(player);
+                } else {
+                    throw new IllegalArgumentException("Invalid file format");
+                }
+                line = reader.readLine();
+            }
+            Storage newStorage = new Storage(players);
+            storage = newStorage;
+            reader.close();
+        } catch (IOException error) {
+            if (error instanceof FileNotFoundException) {
+                System.out.println("File not found");
+            } else {
+                System.out.println("Error reading file");
+            }
+        }
+        return storage;
+    }
 
 }
