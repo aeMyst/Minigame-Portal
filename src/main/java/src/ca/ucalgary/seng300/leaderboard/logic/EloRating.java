f78988cb3394f30019d4dc326102220edfea0c3d
Abrar Maheer Bin Arif <abrarmaheer.arif@ucalgary.ca>
2024-11-13 23:40:23 -0700

    Created the formula for calculating the elo rating

--- src/main/java/src/ca/ucalgary/seng300/leaderboard/logic/EloRating.java
+++ src/main/java/src/ca/ucalgary/seng300/leaderboard/logic/EloRating.java
@@ -7,9 +7,25 @@ public class EloRating implements IEloRating {
     //we're going to calculate new elo ratings from your current elo and the enemy's elo
     @Override
     public int calculateNewElo(int myElo, int enemyElo, boolean won) {
-        //we'll figure out some logic for the formula later
+        int K = 48;  // Constant K-factor used in calculation
+        double expectedScore = 1.0 / (1 + Math.pow(10, (enemyElo - myElo) / 400.0));
 
-        return myElo;
+        int actualScore;
+        if (won) {
+            actualScore = 1;
+        } else {
+            actualScore = 0;
+        }
+
+        // Calculates the new rating
+        int newElo = myElo + (int)(K * (actualScore - expectedScore));
+
+        // Ensures the rating does not go below 0
+        if (newElo < 0) {
+            newElo = 0;
+        }
+
+        return newElo;
     }
 
     //this will be updated after every match
