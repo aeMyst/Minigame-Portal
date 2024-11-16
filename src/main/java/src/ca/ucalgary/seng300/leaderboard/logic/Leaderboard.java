1d96aa6bd5fdfbf193d24b84bd91469dae467ae9
brie <angelika.tanghal@ucalgary.ca>
2024-11-15 17:18:21 -0700

    loadPlayersFromCSV method draft

--- src/main/java/src/ca/ucalgary/seng300/leaderboard/logic/Leaderboard.java
+++ src/main/java/src/ca/ucalgary/seng300/leaderboard/logic/Leaderboard.java
@@ -9,9 +9,46 @@ public class Leaderboard implements ILeaderboard {
 
     @Override
     public void loadPlayersFromCSV(String filename) {
-        //we'll try bufferedReader and parse that values we need from the csv
+        // we'll try bufferedReader and parse that values we need from the csv
 
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            String line;
+            ArrayList<String> storeID = new ArrayList<>();
 
+            while ((line = br.readLine()) != null) {
+                // ignoring blank lines
+                if (line.trim().isEmpty()) {
+                    continue;
+                }
+
+                String[] playerArr = line.split(",");
+
+                // ignore lines with disproportionate amount of data
+                if (playerArr.length != 6) {
+                    continue;
+                }
+
+                String gameType = playerArr[0];
+                String playerID = playerArr[1];
+                storeID.add(playerID);
+                int elo = Integer.parseInt(playerArr[2]);
+                int wins = Integer.parseInt(playerArr[3]);
+                int losses = Integer.parseInt(playerArr[4]);
+                int ties = Integer.parseInt(playerArr[5]);
+
+            }
+
+            // checking for duplicate player IDs
+            Set<String> checkID = new HashSet<>();
+            for (String id : storeID) {
+                if (!checkID.add(id)) {
+                    throw new IllegalArgumentException("ERROR: Duplicate player ID found: " + id);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("ERROR: Failed to read .csv file: " + e.getMessage());
+        }
     }
 
     @Override
