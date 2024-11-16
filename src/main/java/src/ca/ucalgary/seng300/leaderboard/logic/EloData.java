ab754d8aeb0b7fbbdb2a1dda23731c3c43485616
brie <angelika.tanghal@ucalgary.ca>
2024-11-15 17:27:02 -0700

    modified EloData.java constructor for csv reader

--- src/main/java/src/ca/ucalgary/seng300/leaderboard/logic/EloData.java
+++ src/main/java/src/ca/ucalgary/seng300/leaderboard/logic/EloData.java
@@ -3,19 +3,23 @@ package src.ca.ucalgary.seng300.leaderboard.logic;
 
 //useless as of now, we will use Player.java class in the data folder
 public class EloData {
+    private String gameType;
     private String playerID;
     private int elo;
     private int gamesPlayed;
     private int wins;
     private int losses;
 
-    public EloData(String playerID, int elo, int gamesPlayed, int wins, int losses) {
+    public EloData(String gameType, String playerID, int elo, int gamesPlayed, int wins, int losses) {
+        this.gameType = gameType;
         this.playerID = playerID;
         this.elo = elo;
         this.gamesPlayed = gamesPlayed;
         this.wins = wins;
         this.losses = losses;
     }
+
+    public String getGameType() { return gameType; }
 
     public String getPlayerID() {
         return playerID;
@@ -41,4 +45,5 @@ public class EloData {
         return losses;
     }
 }
+
 
