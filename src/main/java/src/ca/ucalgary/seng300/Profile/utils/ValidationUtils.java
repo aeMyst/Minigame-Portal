0975ba8ee13d3eebe1694abdf3f42e023c25889d
tomi <Oluwatomisin.oteniya@ucalgary.ca>
2024-11-17 12:33:08 -0700

    Renamed PasswordUtil -> ValidationUtils.

--- src/main/java/src/ca/ucalgary/seng300/Profile/utils/ValidationUtils.java
+++ src/main/java/src/ca/ucalgary/seng300/Profile/utils/ValidationUtils.java
@@ -1,7 +1,7 @@
 package src.ca.ucalgary.seng300.Profile.utils;
 
-public class PasswordUtil {
+public class ValidationUtils {
 
     // Do we need to hash it? Or we just keep it as plain text?
-    //Testing
+
 }
