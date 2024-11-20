2e7e64d8ffb5efc22195be9f1a9256afd757eb36
brie <angelika.tanghal@ucalgary.ca>
2024-11-19 23:35:14 -0700

    modified Leaderboard.java

--- src/main/java/src/ca/ucalgary/seng300/leaderboard/logic/Leaderboard.java
+++ src/main/java/src/ca/ucalgary/seng300/leaderboard/logic/Leaderboard.java
@@ -4,60 +4,39 @@ import src.ca.ucalgary.seng300.leaderboard.data.Player;
 import src.ca.ucalgary.seng300.leaderboard.data.Storage;
 import src.ca.ucalgary.seng300.leaderboard.interfaces.ILeaderboard;
 import src.ca.ucalgary.seng300.leaderboard.utility.FileManagement;
-
 import java.util.*;
 import java.io.*;
 
 public class Leaderboard implements ILeaderboard {
     private HashMap<String, EloData> players = new HashMap<>();
+    private static final String FILE_PATH = "players_data.csv";
+    private ArrayList<Player> loadPlayers = new ArrayList<>();
 
-    @Override
-    public void loadPlayersFromCSV(String filename) {
-        // we'll try bufferedReader and parse that values we need from the csv
-
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(filename));
-            String line;
-            ArrayList<String> storeID = new ArrayList<>();
-
-            while ((line = br.readLine()) != null) {
-                // ignoring blank lines
-                if (line.trim().isEmpty()) {
-                    continue;
-                }
-
-                String[] playerArr = line.split(",");
-
-                // ignore lines with disproportionate amount of data
-                if (playerArr.length != 6) {
-                    continue;
-                }
-
-                String gameType = playerArr[0];
-                String playerID = playerArr[1];
-                storeID.add(playerID);
-                int elo = Integer.parseInt(playerArr[2].trim());
-                int wins = Integer.parseInt(playerArr[3].trim());
-                int losses = Integer.parseInt(playerArr[4].trim());
-                int ties = Integer.parseInt(playerArr[5].trim());
-
-                EloData playerData = new EloData(gameType, playerID, elo, wins, losses, ties);
-                addPlayer(playerData);
-
-            }
-
-            // checking for duplicate player IDs UNCONFIRMED DRAFT (if anyone asks, lucas said "don't bother")
-
-//            Set<String> checkID = new HashSet<>();
-//            for (String id : storeID) {
-//                if (!checkID.add(id)) {
-//                    throw new IllegalArgumentException("ERROR: Duplicate player ID found: " + id);
-//                }
+//    @Override
+//    public void loadPlayersFromCSV() {
+//        // we'll try bufferedReader and parse that values we need from the csv
+//        File file = new File(FILE_PATH);
+//        Storage storage;
+//
+//        if (file.exists()) {
+//            storage = FileManagement.fileReading(file);
+//            if (storage == null) {
+//                storage = new Storage();
+//                System.out.println("[ERROR] Failed to read file. Starting with an empty player list.");
+//            } else {
+//                loadPlayers = storage.getPlayers();
+//                System.out.println("Loading existing player data from file.");
 //            }
-        } catch (IOException e) {
-            System.err.println("ERROR: Failed to read .csv file: " + e.getMessage());
-        }
-    }
+//        } else {
+//            storage = new Storage();
+//            System.out.println("[ERROR] No existing file found. Starting with an empty player list.");
+//        }
+//
+//        for (Player player : loadPlayers) {
+//            EloData eloPlayer = new EloData(player.getGameType(), player.getPlayerID(), player.getElo(), player.get)
+//        }
+//
+//    }
 
     @Override
     public void addPlayer(EloData playerID) {
@@ -67,47 +46,37 @@ public class Leaderboard implements ILeaderboard {
     // sorts names with corresponding elo values in order
     @Override
     public List<List<String>> sortLeaderboard(String gameType) {
-
-        String FILE_PATH = "players_data.csv";
-        File file = new File(FILE_PATH);
-
-        Storage storage = FileManagement.fileReading(file);
-
-        List<Player> neededPlayers = new ArrayList<>();
-
         List<List<String>> sortedLeaderboard = new ArrayList<>();
+        File file = new File(FILE_PATH);
+        Storage storage;
 
-        for (Player player : storage.getPlayers()) {
-            String game = player.getGameType();
-            if (game.equals(gameType)) {
-                neededPlayers.add(player);
+        if (file.exists()) {
+            storage = FileManagement.fileReading(file);
+
+            List<Player> neededPlayers = new ArrayList<>();
+
+
+            for (Player player : storage.getPlayers()) {
+                String game = player.getGameType();
+                if (game.equals(gameType)) {
+                    neededPlayers.add(player);
+                }
             }
+
+            neededPlayers.sort((player1, player2) -> Integer.compare(player2.getElo(), player1.getElo()));
+
+            for (Player player : neededPlayers) {
+                List<String> entry = new ArrayList<>();
+                entry.add(player.getPlayerID());
+                entry.add(String.valueOf(player.getElo()));
+                entry.add(String.valueOf(player.getWins()));
+                sortedLeaderboard.add(entry);
+            }
+        } else {
+            System.out.println("[ERROR] File does not exist.");
         }
 
-        neededPlayers.sort((player1, player2) -> Integer.compare(player2.getElo(), player1.getElo()));
 
-        for (Player player : neededPlayers) {
-            List<String> entry = new ArrayList<>();
-            entry.add(player.getPlayerID());
-            entry.add(String.valueOf(player.getElo()));
-            entry.add(String.valueOf(player.getWins()));
-            sortedLeaderboard.add(entry);
-        }
-
-        // separate leaderboards for each game not yet created
-
-
-        //for (){}
-        /*
-        List<HashMap.Entry<String, EloData>> sortPlayers = new ArrayList<>(players.entrySet());
-        // sorts the players according to descending elo ratings
-        sortPlayers.sort((player1, player2) -> Integer.compare(player2.getValue().getElo(), player1.getValue().getElo()));
-
-        // adds each player from the sorted list to another list with the appropriate data type to be returned
-        for (HashMap.Entry<String, EloData> entry : sortPlayers) {
-            sortedLB.add(entry.getValue());
-        }
-        */
         return sortedLeaderboard;
     }
 }
