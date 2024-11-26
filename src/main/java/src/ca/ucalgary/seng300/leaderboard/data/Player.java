2dc0730ecf25d0266fd15ab06aff178e59108178
Lucas Li <xue.li2@ucalgary.ca>
2024-11-25 17:41:37 -0700

    Merged Main to leaderboard for functionality development

---
+++ src/main/java/src/ca/ucalgary/seng300/leaderboard/data/Player.java
@@ -0,0 +1,78 @@
+package src.ca.ucalgary.seng300.leaderboard.data;
+
+public class Player {
+    private String gameType;
+    private String playerID;
+    private int elo;
+    private int wins;
+    private int losses;
+    private int ties;
+
+    public Player(String gameType, String playerID, int elo, int wins, int losses, int ties) {
+        this.gameType = gameType;
+        this.playerID = playerID;
+        this.elo = elo;
+        this.wins = wins;
+        this.losses = losses;
+        this.ties = ties;
+    }
+
+    public void setGameType(String gameType) {
+        this.gameType = gameType;
+    }
+
+    public String getGameType() {
+        return gameType;
+    }
+
+    public void setPlayerID(String playerID) {
+        this.playerID = playerID;
+    }
+
+    public String getPlayerID() {
+        return playerID;
+    }
+
+    public void setElo(int elo) {
+        this.elo = elo;
+    }
+
+    public int getElo() {
+        return elo;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setTies(int ties) {
+        this.ties = ties;
+    }
+
+    public int getTies() {
+        return ties;
+    }
+
+    @Override
+    public String toString() {
+        return "GameType: " + gameType +
+                "\tPlayerID: " + playerID +
+                "\tElo: " + elo +
+                "\tWins: " + wins +
+                "\tLosses: " + losses +
+                "\tTies: " + ties;
+    }
+
+}
--- src/main/java/src/ca/ucalgary/seng300/leaderboard/interfaces/ILeaderboard.java
+++ src/main/java/src/ca/ucalgary/seng300/leaderboard/interfaces/ILeaderboard.java
@@ -1,14 +1,6 @@
 package src.ca.ucalgary.seng300.leaderboard.interfaces;
 
-import src.ca.ucalgary.seng300.leaderboard.logic.EloData;
-
-import java.util.List;
-
 public interface ILeaderboard {
-    //we will be taking (String "whatever file path")
-    void loadPlayersFromCSV();
 
-    void addPlayer(EloData playerID);
-
-    List<EloData> sortedLeaderboard();
+    String[][] sortLeaderboard(String gameType);
 }
