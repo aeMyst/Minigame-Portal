f872fceda8efcf4a4a45539ca4a5452e40c8541b
brie <angelika.tanghal@ucalgary.ca>
2024-11-26 21:17:32 -0700

    30188569: modified LB interface

--- src/main/java/src/ca/ucalgary/seng300/leaderboard/interfaces/ILeaderboard.java
+++ src/main/java/src/ca/ucalgary/seng300/leaderboard/interfaces/ILeaderboard.java
@@ -3,4 +3,10 @@ package src.ca.ucalgary.seng300.leaderboard.interfaces;
 public interface ILeaderboard {
 
     String[][] sortLeaderboard(String gameType);
+
+    String[][] getC4Leaderboard();
+
+    String[][] getTicTacToeLeaderboard();
+
+    String[][] getCheckersLeaderboard();
 }
