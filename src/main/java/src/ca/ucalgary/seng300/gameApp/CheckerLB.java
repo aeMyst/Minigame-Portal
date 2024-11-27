324ea2413b73b5cabc4b697d701a8c0716f192a3
Brie <angelika.tanghal@ucalgary.ca>
2024-11-27 11:36:17 -0700

    30188569: modified Checkers leaderboard display

--- src/main/java/src/ca/ucalgary/seng300/gameApp/CheckerLB.java
+++ src/main/java/src/ca/ucalgary/seng300/gameApp/CheckerLB.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
@@ -52,41 +53,53 @@ public class CheckerLB implements IScreen {
     }
 
     private VBox createLeaderboardEntries(String[][] data) {
-        VBox entriesBox = new VBox(10);
+        int lastEntry = data.length - 1;
+        int count = 0;
+
+        VBox entriesBox = new VBox(5);
         entriesBox.setAlignment(Pos.CENTER);
         entriesBox.setPadding(new Insets(10));
+        entriesBox.setMaxWidth(420);
+        entriesBox.setStyle("-fx-border-color: grey; -fx-border-width: 2; -fx-border-radius: 10 10 10 10;");
 
         HBox headerBox = new HBox(10);
         headerBox.setAlignment(Pos.CENTER);
         headerBox.setMaxWidth(400);
         headerBox.setPrefWidth(400);
-        headerBox.setStyle("-fx-background-color: lightgray; -fx-padding: 10;");
+        headerBox.setStyle("-fx-background-color: grey; -fx-padding: 10; -fx-background-radius: 10 10 0 0;");
 
         Label nameHeader = new Label("NAME");
-        nameHeader.setFont(Font.font("Arial", FontWeight.BOLD,20));
+        nameHeader.setFont(Font.font("Arial",FontWeight.BOLD,20));
         nameHeader.setPrefWidth(220);
         nameHeader.setAlignment(Pos.CENTER);
+        nameHeader.setTextFill(Color.WHITE);
 
         Label eloHeader = new Label("SCORE");
         eloHeader.setFont(Font.font("Arial",FontWeight.BOLD,20));
         eloHeader.setPrefWidth(90);
         eloHeader.setAlignment(Pos.CENTER);
+        eloHeader.setTextFill(Color.WHITE);
 
         Label winsHeader = new Label("WINS");
         winsHeader.setFont(Font.font("Arial",FontWeight.BOLD,20));
         winsHeader.setPrefWidth(80);
         winsHeader.setAlignment(Pos.CENTER);
+        winsHeader.setTextFill(Color.WHITE);
 
         headerBox.getChildren().addAll(nameHeader,eloHeader,winsHeader);
         entriesBox.getChildren().add(headerBox);
 
         for (String[] entry : data) {
-            HBox entryBox = new HBox(5);
+            HBox entryBox = new HBox(10);
             entryBox.setSpacing(40);
-            entryBox.setStyle("-fx-padding: 5; -fx-border-color: lightgrey; -fx-border-width: 1;");
+            entryBox.setStyle("-fx-padding: 5; -fx-background-color: lightgrey;");
             entryBox.setPrefWidth(400);
             entryBox.setMinWidth(400);
             entryBox.setMaxWidth(400);
+            if (count == lastEntry) {
+                entryBox.setStyle("-fx-padding: 5; -fx-background-color: lightgrey; -fx-background-radius: 0 0 10 10;");
+
+            }
 //            entryBox.setAlignment(Pos.BASELINE_CENTER);
             entryBox.setAlignment(Pos.BASELINE_LEFT);
 
@@ -108,6 +121,8 @@ public class CheckerLB implements IScreen {
 
             entryBox.getChildren().addAll(playerLabel, eloLabel, winsLabel);
             entriesBox.getChildren().add(entryBox);
+
+            count++;
         }
         return entriesBox;
     }
