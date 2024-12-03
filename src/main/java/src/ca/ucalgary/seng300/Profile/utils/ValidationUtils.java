21586c2f3b6baf877425ea0d500f2940ed3bc20a
tomi <Oluwatomisin.oteniya@ucalgary.ca>
2024-12-02 22:08:25 -0700

    30204347 - Hopefully fixed email regex. For some reason can't run tests

--- src/main/java/src/ca/ucalgary/seng300/Profile/utils/ValidationUtils.java
+++ src/main/java/src/ca/ucalgary/seng300/Profile/utils/ValidationUtils.java
@@ -16,7 +16,8 @@ public class ValidationUtils {
     
     
     //Basic email validation regex pattern.
-    private static final String email_Regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private static final String email_Regex = "^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$";
+    //Found the regex in: https://support.boldsign.com/kb/article/15962/how-to-create-regular-expressions-regex-for-email-address-validation
 
 
     private static final String userName_Regex = "^[a-zA-Z0-9]+$"; //Basic username Regex patern.
