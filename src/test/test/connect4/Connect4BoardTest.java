bc513769ede9b5afbad2328f717a5b558fa5d011
Shezan Nanji <shezan.nanji@ucalgary.ca>
2024-12-03 21:14:31 -0700

    30174900: added tests for the Connect4Board

---
+++ src/test/test/connect4/Connect4BoardTest.java
@@ -0,0 +1,59 @@
+package Connect4;
+
+import org.junit.jupiter.api.Test;
+import src.ca.ucalgary.seng300.gamelogic.Connect4.Connect4Board;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class Connect4BoardTest {
+    @Test
+    public void TestConstructorInit(){
+        Connect4Board board = new Connect4Board();
+        int[][] board_array = board.getBoard();
+
+        for (int i=0; i<6; i++){
+            for (int j=0; j<7; j++){
+                // Assert all cells are initialized to 0
+                assertEquals(0, board_array[i][j]);
+            }
+        }
+    }
+
+    @Test
+    public void TestPrintBoard(){
+        Connect4Board board= new Connect4Board();
+        int[][] board_array = board.getBoard();
+        // Redirect System.out to capture output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        // Call printBoard and restore original System.out
+        Board.printBoard(board_array);
+        System.setOut(originalOut);
+
+        // Verify the output matches the expected printed board format
+        String expectedOutput =
+                "   1   2   3   4   5   6   7   \n" +
+                        " -----------------------------   \n" +
+                        " | 0 | 0 | 0 | 0 | 0 | 0 | 0 | \n" +
+                        " -----------------------------   \n" +
+                        " | 0 | 0 | 0 | 0 | 0 | 0 | 0 | \n" +
+                        " -----------------------------   \n" +
+                        " | 0 | 0 | 0 | 0 | 0 | 0 | 0 | \n" +
+                        " -----------------------------   \n" +
+                        " | 0 | 0 | 0 | 0 | 0 | 0 | 0 | \n" +
+                        " -----------------------------   \n" +
+                        " | 0 | 0 | 0 | 0 | 0 | 0 | 0 | \n" +
+                        " -----------------------------   \n" +
+                        " | 0 | 0 | 0 | 0 | 0 | 0 | 0 | \n" +
+                        " -----------------------------   \n";
+
+        assertEquals(expectedOutput.replace("\r\n", "\n"), outputStream.toString().replace("\r\n", "\n"));
+    }
+
+
+}
