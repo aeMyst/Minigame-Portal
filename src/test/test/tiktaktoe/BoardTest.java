c5fdc535c17f81918286e201c6e3d0778b9554aa
Alexis Newell <mmnewell@me.com>
2024-12-03 22:29:36 -0700

    30205618: junit tests

---
+++ src/test/test/tiktaktoe/BoardTest.java
@@ -0,0 +1,52 @@
+package src.ca.ucalgary.seng300.tictactoe;
+
+import src.ca.ucalgary.seng300.gamelogic.tictactoe.Board;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BoardTest {
+    @Test
+    public void TestConstructorInit(){
+        Board board = new Board();
+        char[][] board_array = board.getBoard();
+
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                // Assert all cells are initialized to ' '
+                assertEquals(' ', board_array[i][j]);
+            }
+        }
+    }
+
+    @Test
+    public void TestPrintBoard(){
+        Board board= new Board();
+        char[][] board_array = board.getBoard();
+        // Redirect System.out to capture output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        // Call printBoard and restore original System.out
+        Board.printBoard(board_array);
+        System.setOut(originalOut);
+
+        // Verify the output matches the expected printed board format
+        String expectedOutput =
+                "      1       2       3   \n" +
+                        "    -----------------------\n" +
+                        "1  |      |      |      |  \n" +
+                        "    -----------------------\n" +
+                        "2  |      |      |      |  \n" +
+                        "    -----------------------\n" +
+                        "3  |      |      |      |  \n" +
+                        "    -----------------------\n";
+
+        assertEquals(expectedOutput.replace("\r\n", "\n"), outputStream.toString().replace("\r\n", "\n"));
+    }
+
+}
