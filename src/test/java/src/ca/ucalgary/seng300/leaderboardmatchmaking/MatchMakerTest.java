e66b958e415770313173e3e5fcc7dfeab575d94d
Arfa Raja <arfa.raja@ucalgary.ca>
2024-12-03 18:08:39 -0700

    30177290 she java on my intellij till i

--- src/test/java/src/ca/ucalgary/seng300/leaderboardmatchmaking/MatchMakerTest.java
+++ src/test/java/src/ca/ucalgary/seng300/leaderboardmatchmaking/MatchMakerTest.java
@@ -91,7 +91,7 @@ public class MatchMakerTest {
         Storage storage = new Storage();
         MatchMaker matchMaker = new MatchMaker(storage);
         Player player1 = new Player("CONNECT4", "Player1", 1000, 10, 5, 0);
-        Player player2 = new Player("TIKTAKT", "Player2", 1100, 15, 7, 0);
+        Player player2 = new Player("TIKTAKTOE", "Player2", 1100, 15, 7, 0);
         storage.addPlayer(player1);
         storage.addPlayer(player2);
 
