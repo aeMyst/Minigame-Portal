9effc994071611048e8dea5a516dc2d8c8f6f3e9
Brie <angelika.tanghal@ucalgary.ca>
2024-12-03 18:53:23 -0700

    30188569: added javadoc comments for another writing method

--- src/main/java/src/ca/ucalgary/seng300/leaderboard/utility/FileManagement.java
+++ src/main/java/src/ca/ucalgary/seng300/leaderboard/utility/FileManagement.java
@@ -129,12 +129,17 @@ public class FileManagement {
         }
     }
 
+    /**
+     * Method that writes a new file with data from match history
+     * @param file File used to contain the data from match history
+     * @param storage Storage containing the HistoryPlayer objects to be written in the new file
+     */
     public static void fileWritingHistoryNewFile(File file, HistoryStorage storage) {
         try (BufferedWriter writerBuffer = new BufferedWriter(new FileWriter(file))) {
             //write info to the file in the format of "gametype, player_id, winner, loser, eloGained, eloLost"
             for (HistoryPlayer hp : storage.getPlayersHistory()) {
-                String[] player = {hp.getGameTypeHistory(), hp.getPlayerIDHistory(), hp.getWinnerString(), hp.getLoserString(), String.valueOf(hp.getEloGained()), String.valueOf(hp.getEloLost()), hp.getDate()};
-                writerBuffer.write(String.join(",", player));
+                String[] player = {hp.getGameTypeHistory(), hp.getPlayerIDHistory(), hp.getWinnerString(), hp.getLoserString(), String.valueOf(hp.getEloGained()), String.valueOf(hp.getEloLost()), hp.getDate()};  // fetch the attributes of each HistoryPlayer and add them to a String array
+                writerBuffer.write(String.join(",", player));   // write the string array to the file with each element separated by a comma
                 writerBuffer.newLine();
             }
 
