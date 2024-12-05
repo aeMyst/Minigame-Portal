9d3d9c86685f7efd87b2b7f47e2fb33742dfc189
Peter <peter.tran2@ucalgary.ca>
2024-12-05 04:32:53 -0700

    30160145 - leaderboard conflict merge solved

--- src/main/java/src/ca/ucalgary/seng300/Profile/interfaces/ProfileInterface.java
+++ src/main/java/src/ca/ucalgary/seng300/Profile/interfaces/ProfileInterface.java
@@ -4,20 +4,17 @@ import src.ca.ucalgary.seng300.Profile.models.User;
 import src.ca.ucalgary.seng300.Profile.models.Profile;
 
 /**
- * Interface to define methods implemented in ProfileService and ProfileController to handle profile information
- * @author
- */
+
+ Interface to define methods implemented in ProfileService and ProfileController to handle profile information
+ @author*/
 public interface ProfileInterface {
     // Initializing string field
     String viewProfile();
-
     // Method to update profile information
     void updateProfile(User user, String newUsername, String newEmail, String newPassword);
-
-    // Method to track game history
-    void trackGameHistory(User user, Profile profile);
-
-    // Method to update ranking
-    void updateRanking(User user, int rank);
+    //Method to initialize profile for new user
+    void initializeProfile(String username);
+    //Method to search user's profile
+    String searchProfile(String username);
 
 }
