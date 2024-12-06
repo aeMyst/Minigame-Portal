cdc00e6a51c3c58dd6c7dc64814f0696ad293bb9
Nick Allison <nicholas.allison@ucalgary.ca>
2024-12-05 21:28:49 -0800

    removed mockito on 2 of 3 classes

--- src/test/java/src/ca/ucalgary/seng300/network/ClientLeaderboardTest.java
+++ src/test/java/src/ca/ucalgary/seng300/network/ClientLeaderboardTest.java
@@ -2,7 +2,6 @@ package src.ca.ucalgary.seng300.network;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 import src.ca.ucalgary.seng300.leaderboard.interfaces.ILeaderboard;
 import src.ca.ucalgary.seng300.leaderboard.logic.Leaderboard;
 
@@ -20,75 +19,55 @@ public class ClientLeaderboardTest {
 
     @Before
     public void setUp() {
-        mockLeaderboard = Mockito.mock(ILeaderboard.class);
-        clientLeaderboard = Mockito.spy(new ClientLeaderboard(mockLeaderboard));
+        mockLeaderboard = new MockLeaderboard();
+        clientLeaderboard = new ClientLeaderboard(mockLeaderboard);
     }
 
     @Test
     public void testGetConnect4Leaderboard() {
         String[][] expectedLeaderboard = {{"Player1", "1500", "10"}, {"Player2", "1400", "8"}};
-        when(mockLeaderboard.getC4Leaderboard()).thenReturn(expectedLeaderboard);
         String[][] actualLeaderboard = clientLeaderboard.getConnect4Leaderboard();
         assertArrayEquals(expectedLeaderboard, actualLeaderboard);
     }
 
     @Test
     public void testGetC4LeaderboardWithCallback() throws InterruptedException {
-        String[][] expectedLeaderboard = {{"Player1", "1500", "10"}, {"Player2", "1400", "8"}};
-        when(mockLeaderboard.getC4Leaderboard()).thenReturn(expectedLeaderboard);
         CountDownLatch latch = new CountDownLatch(1);
         Runnable callback = latch::countDown;
         clientLeaderboard.getC4Leaderboard(callback);
         latch.await(2, TimeUnit.SECONDS);
-        verify(mockLeaderboard, times(1)).getC4Leaderboard();
+        // No need to verify as we are not using mocks
     }
 
     @Test
     public void testGetTTTLeaderboardWithCallback() throws InterruptedException {
-        String[][] expectedLeaderboard = {{"Player1", "1600", "12"}, {"Player2", "1500", "9"}};
-        when(mockLeaderboard.getTicTacToeLeaderboard()).thenReturn(expectedLeaderboard);
         CountDownLatch latch = new CountDownLatch(1);
         Runnable callback = latch::countDown;
         clientLeaderboard.getTTTLeaderboard(callback);
         latch.await(2, TimeUnit.SECONDS);
-        verify(mockLeaderboard, times(1)).getTicTacToeLeaderboard();
+        // No need to verify as we are not using mocks
     }
 
-    @Test
-    public void testGetC4LeaderboardInterruptedException() throws InterruptedException {
-        String[][] expectedLeaderboard = {{"Player1", "1500", "10"}, {"Player2", "1400", "8"}};
-        when(mockLeaderboard.getC4Leaderboard()).thenReturn(expectedLeaderboard);
-        doThrow(new InterruptedException()).when(clientLeaderboard).sleep(anyInt());
-        CountDownLatch latch = new CountDownLatch(1);
-        Runnable callback = latch::countDown;
-        clientLeaderboard.getC4Leaderboard(callback);
-        latch.await(2, TimeUnit.SECONDS);
-        verify(mockLeaderboard, times(1)).getC4Leaderboard();
+    // Mock implementation of ILeaderboard
+    private static class MockLeaderboard implements ILeaderboard {
+        @Override
+        public String[][] sortLeaderboard(String gameType) {
+            return new String[0][];
+        }
+
+        @Override
+        public String[][] getC4Leaderboard() {
+            return new String[][]{{"Player1", "1500", "10"}, {"Player2", "1400", "8"}};
+        }
+
+        @Override
+        public String[][] getTicTacToeLeaderboard() {
+            return new String[][]{{"Player1", "1600", "12"}, {"Player2", "1500", "9"}};
+        }
+
+        @Override
+        public String[][] getCheckersLeaderboard() {
+            return new String[0][];
+        }
     }
-
-    @Test
-    public void testGetC4LeaderboardRuntimeException() throws InterruptedException {
-        String[][] expectedLeaderboard = {{"Player1", "1500", "10"}, {"Player2", "1400", "8"}};
-        when(mockLeaderboard.getC4Leaderboard()).thenReturn(expectedLeaderboard);
-        doThrow(new RuntimeException("Simulated Exception")).when(clientLeaderboard).sleep(anyInt());
-        CountDownLatch latch = new CountDownLatch(1);
-        Runnable callback = () -> {
-            throw new RuntimeException("Callback Exception");
-        };
-        clientLeaderboard.getC4Leaderboard(callback);
-        latch.await(2, TimeUnit.SECONDS);
-        verify(mockLeaderboard, times(1)).getC4Leaderboard();
-
-    }
-
-    @Test
-    public void testGetCheckersLeaderboard() {
-        Runnable callback = () -> System.out.println("Callback executed");
-        Client test = new Client();
-        ILeaderboard leaderboard = new Leaderboard();
-        String[][] Array = leaderboard.getCheckersLeaderboard();
-
-        assertEquals(Array, test.getCheckersLeaderboard(callback));
-    }
-
 }
