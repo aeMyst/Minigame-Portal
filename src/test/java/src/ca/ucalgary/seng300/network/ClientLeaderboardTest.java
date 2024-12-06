ee6f1ab1b3dba5a30c86d30c9047a8f709e9733d
Nick Allison <nicholas.allison@ucalgary.ca>
2024-12-05 21:55:17 -0800

    30119591 - removed mockito fully

--- src/test/java/src/ca/ucalgary/seng300/network/ClientLeaderboardTest.java
+++ src/test/java/src/ca/ucalgary/seng300/network/ClientLeaderboardTest.java
@@ -10,7 +10,6 @@ import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
 
 public class ClientLeaderboardTest {
 
